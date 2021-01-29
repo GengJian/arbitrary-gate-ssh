768ea24c6beca16bc77eb9c4be28238fc0e70c74
Soul <SoulStayReal@gmail.com>
2021-01-29 13:12:45 +0800

    UI - 增加日志输出

--- ArbitraryGateSSH/LogOutputController.swift
+++ ArbitraryGateSSH/LogOutputController.swift
@@ -10,12 +10,35 @@ import Cocoa
 
 class LogOutputController: NSViewController {
     
+    @IBOutlet var textView: NSTextView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
+        
+        self.textView.isEditable = false
+        
+        self.show(log: "✈️ Welcome to wenzhou airport ")
+        
     }
     
-    func show(logString: String) -> Void {
+    func show(log newstr: String) -> Void {
+        
+        let dateformatter = DateFormatter()
+        dateformatter.dateFormat = "【YYYY-MM-dd HH:mm:ss】"// 自定义时间格式
+        let now = dateformatter.string(from: Date())
+        
+        let resLog = self.textView.string
+            .appending("\n")
+            .appending(now)
+            .appending("\n")
+            .appending(newstr)
+            .appending("\n")
+    
+        self.textView.string = resLog
+        // 日志自动滚动到最底部
+//        let range = resLog.range(of: newstr)
+//        let nsrange = NSRange.init(resLog, in: resLog)
+        self.textView.scrollRangeToVisible(NSRange.init(location: newstr.count-1, length: 1))
         
     }
     
