89ce152e6b7a7febd3c452dfc8a6fe0d3a5467bd
Soul <SoulStayReal@gmail.com>
2021-01-29 10:50:22 +0800

    Init - 新建 Mac OS 应用

---
+++ ArbitraryGateSSH/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ArbitraryGateSSH
+//
+//  Created by Soul on 2021/1/29.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
